c9d6bf980bb2ba46bc4a73d89934afa7b4068dfc
Davisralves <davialves324@gmail.com>
2022-07-13 11:40:29 -0300

    criando classes para tasks e listas

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,32 @@
-console.log("hello world");
+var Task = /** @class */ (function () {
+    function Task(name) {
+        this.name = name;
+    }
+    Task.prototype.editTask = function (newTask) {
+        this.name = newTask;
+    };
+    return Task;
+}());
+var ToDoList = /** @class */ (function () {
+    function ToDoList() {
+        this.tasks = [];
+    }
+    ToDoList.prototype.addTask = function (newTask) {
+        this.tasks.push(newTask);
+    };
+    ToDoList.prototype.removeTask = function (task) {
+        var index = this.tasks.indexOf(task, 1);
+        this.tasks.splice(index, 1);
+    };
+    return ToDoList;
+}());
+var list = new ToDoList();
+var task1 = new Task("Wash car");
+console.log("task:", task1);
+console.log("list:", list);
+list.addTask(task1);
+list.addTask(task1);
+console.log("list:", list);
+list.removeTask(task1);
+console.log("list:", list);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,34 @@
-console.log("hello world")
+class Task {
+	constructor(public name: string) {}
+
+	editTask(newTask: string): void {
+		this.name = newTask;
+	}
+}
+
+class ToDoList {
+	public tasks: Task[];
+
+	constructor() {
+		this.tasks = [];
+	}
+
+	addTask(newTask: Task) {
+		this.tasks.push(newTask);
+	}
+
+	removeTask(task: Task) {
+		const index = this.tasks.indexOf(task, 1);
+		this.tasks.splice(index, 1);
+	}
+}
+
+const list = new ToDoList();
+const task1 = new Task("Wash car");
+console.log("task:", task1);
+console.log("list:", list);
+list.addTask(task1);
+list.addTask(task1);
+console.log("list:", list);
+list.removeTask(task1);
+console.log("list:", list);
